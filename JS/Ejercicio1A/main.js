9993da80f37660546f0d20a6a60947fdeb81390a
Gajuan99 <gajuan99@gmail.com>
2025-01-30 20:36:26 +0100

    Ejercicios JavaScript
    Ejercicios js hasta el 1C

---
+++ JS/Ejercicio1A/main.js
@@ -0,0 +1,11 @@
+// Utilización de variables
+
+const lado1 = 16
+const lado2 = 23
+const altura = 13
+
+const areaCubo = lado1 * lado2
+const volumenCubo = areaCubo * altura
+
+console.log('El área del cubo es igual a ' + areaCubo + ' unidades')
+console.log('El el volumen del cubo es igual a ' + volumenCubo + ' unidades')
---
+++ JS/Ejercicio1B/main.js
@@ -0,0 +1,6 @@
+const cumpleanos = {
+    dia: 4,
+    mes: 'febrero'
+}
+
+console.log ('Mi cumpleaños es el día ' + cumpleanos.dia + ' de ' + cumpleanos.mes)
